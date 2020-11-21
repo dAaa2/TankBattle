fc78593e07a5ba1a2c385bce81fe43a6fd91d3ec
Aaa2 <13518410056@163.com>
2020-11-21 11:01:30 +0800

    change main.cpp

--- TANKBATTLE/main.cpp
+++ TANKBATTLE/main.cpp
@@ -22,35 +22,10 @@ int main() {
 	BeginBatchDraw();
 	g1.showmap();
 	
-	/*while (true) {
+	while (true) {
 		g1.showmap();
 		FlushBatchDraw();
 		Sleep(20);
-		cleardevice();
-	}*/
-	Player p1(50, 50, Up);
-	BulletLink* BL = new BulletLink();
-	int time = 0;
-	while (true) {
-		g1.showmap();
-		if (p1.Is_keydown()) {
-			keydown key = p1.InputCheck();
-			if (key == KEY_SHOOT && time == 0) {
-				p1.shoot(BL);
-			}		
-		}
-		BulletLink* p = BL;
-		while (p->next != nullptr) {
-			p->next->NewBullet.move();
-			putimage(p->next->NewBullet.GetCoord_X(), p->next->NewBullet.GetCoord_Y(), &p->next->NewBullet.Bullet_pic[p->next->NewBullet.GetDir()]);
-			
-			p = p->next;
-		}
-		if (time < 2)
-			time++;
-		else time = 0;
-		FlushBatchDraw();
-		Sleep(33);
 		cleardevice();
 	}
 	
